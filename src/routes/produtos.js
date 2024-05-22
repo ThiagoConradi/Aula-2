e086b2e63a607a46683be37830136fb27cf05bd4
BERNARDO BECHI DA SILVA <bernardo_b_silva@estudante.sesisenai.org.br>
2024-05-21 21:47:15 -0300

    parte do codigo do Bernardo

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -13,6 +13,12 @@ router.get('/', produtosController.list);
 // Rota para mostrar um produto (READ)
 router.get('/:id', produtosController.show);
 
+// Rota para atualizar um produto (UPDATE)
+router.get('/:id/edit', produtosController.formEdit);
+
+// Rota para atualizar um produto (UPDATE)
+router.post('/:id/edit', produtosController.update);
+
 // Rota para deletar um produto específico (DELETE)
 router.post('/delete/:id', produtosController.delete);
 
